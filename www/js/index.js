a0c262eafe474635e532e73bab8dd2f955cf50f3
javierbyte <hola@javierbyte.com>
2014-04-02 20:52:55 -0700

    added querystring

--- www/js/index.js
+++ www/js/index.js
@@ -3,13 +3,22 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 cordova.plugins.barcodeScanner.scan(
       function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
+        var data = result.split('&');
+        var result = [];
+        for(x=0;x<data.length) {
+          result[x] = data[x].split('=');
+          alert('el resultado de ' + result[x][0] + ' es ' + result[x][1]);
+        }
       }, 
       function (error) {
           alert("Scanning failed: " + error);
       }
    );
 }
+
+/*
+  re rfc del emisor
+  rr rfc del receptor
+  tt cantidad
+  id numero de la factura
+*/
