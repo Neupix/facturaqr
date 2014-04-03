16f4f26830d9945ce30cf1bdafd82522e34b6c53
javierbyte <hola@javierbyte.com>
2014-04-02 20:35:42 -0700

    init

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+}
