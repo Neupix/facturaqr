2737621cfcd98372e92b1aba07aff8f91ab8ca1e
javierbyte <hola@javierbyte.com>
2014-04-02 21:58:41 -0700

    fix ui

--- www/js/index.js
+++ www/js/index.js
@@ -12,12 +12,11 @@ function scan() {
         var each = [];
         for(x=0;x<data.length;x++) {
           each[x] = data[x].split('=');
-          getElementById(each[x][0]).innerHTML = each[x][1];
-
-          getElementById('info').style.display = "none";
-          getElementById('data').style.display = "block";
-          getElementById('startBtn').innerHTML = "Escanear otra vez";
+          document.getElementById(each[x][0]).innerHTML = each[x][1];
         }
+        document.getElementById('info').style.display = "none";
+        document.getElementById('data').style.display = "block";
+        document.getElementById('startBtn').innerHTML = "Escanear otra vez";
       }, 
       function (error) {
           alert("Scanning failed: " + error);
