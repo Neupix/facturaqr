b36c399d8f07c9ceb4a21e38fa35bda35c81d67f
javierbyte <hola@javierbyte.com>
2014-04-03 01:45:37 -0700

    better email

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,8 @@ function scan() {
               var price = each[x][1].split('.');
   
               //the we transform the integer part into a real INT, and then take only the first two cent characters
-              document.getElementById(each[x][0]).innerHTML = '$' + parseInt(price[0],10) + '.' + price[1].substring(0,2);
+              var stringPrice = '$' + parseInt(price[0],10) + '.' + price[1].substring(0,2);
+              document.getElementById(each[x][0]).innerHTML = stringPrice;
             }
 
           } else {
@@ -54,7 +55,9 @@ function scan() {
 
 function email() {
   var userEmail = prompt("¿Cuál es tu email?");
-  var mailToUrl = "mailto:" + userEmail + "?subject=Factura:" + userEmail + "&Body=BODY";
+  var bodyMsj = "RFC del emisor: " + each[0][1] + "\nRFC del receptor: " + each[1][1] + "\nCantidad: " + stringPrice + "\n ID de la factura: " + each[3][1] + "\n\n Email generado por Factura QR, de http://neupix.com/";
+  bodyMsj = encodeURIComponent(bodyMsj.trim());
+  var mailToUrl = "mailto:" + userEmail + "?subject=Factura:%20" + each[3][1] + "&Body=BODY";
   window.location = mailToUrl;
   console.log(mailToUrl);
 }
